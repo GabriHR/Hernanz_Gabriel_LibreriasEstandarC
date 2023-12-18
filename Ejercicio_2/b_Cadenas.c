6e98cb306e902ee63d612ac0f5f8cae8864cde93
GabriHR <ghernrod@myuax.com>
2023-12-18 14:46:34 +0100

    Funcion main apartado b ejercicio 2.

--- Ejercicio_2/b_Cadenas.c
+++ Ejercicio_2/b_Cadenas.c
@@ -1,2 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+
+int main() {
+    char cadena[] = "Hola Ruben. Felices fiestas.";
+    int longitud = strlen(cadena);
+    printf("La longitud de la cadena es: %d\n", longitud);
+    return 0;
+}
