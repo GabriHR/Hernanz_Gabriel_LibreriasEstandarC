5af2d0114e451f29645bde7c3abb43de6287b1cc
GabriHR <ghernrod@myuax.com>
2023-12-18 13:24:09 +0100

    Función main

--- Ejercicio_1/a_FuncionesComunes.c
+++ Ejercicio_1/a_FuncionesComunes.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main() {
+    int numeros[] = {10, -4, 0, -18, 30};
+    int longitud = sizeof(numeros) / sizeof(numeros[0]);
+
+    for(int i = 0; i < longitud; i++) {
+        int valor_absoluto = abs(numeros[i]);
+        printf("El valor absoluto de %d es: %d\n", numeros[i], valor_absoluto);
+    }
+
+    return 0;
+}
