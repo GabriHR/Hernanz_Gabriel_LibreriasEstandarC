00005bb4f972547d581255ab9e7ae34a4891f200
GabriHR <ghernrod@myuax.com>
2023-12-18 13:26:04 +0100

    Funcion main apartado b) ejercicio 1

--- Ejercicio_1/b_FuncionesComunes.c
+++ Ejercicio_1/b_FuncionesComunes.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+int main() {
+    int numero_aleatorio = rand();
+    printf("Numero aleatorio sin cambiar la semilla: %d\n", numero_aleatorio);
+
+    srand(time(NULL));
+
+    numero_aleatorio = rand();
+    printf("Numero aleatorio despues de cambiar la semilla: %d\n", numero_aleatorio);
+
+    return 0;
+}
